ec3067b366fa629ad8d800e24685027f77d0ee96
Abilio Esteves <abilio.esteves@ibm.com>
2019-01-02 16:58:56 -0200

    adjusted import of logrus

--- gohclient/gohclient.go
+++ gohclient/gohclient.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 // API defines an interface for helper methods that encapsulates http requests complexities
