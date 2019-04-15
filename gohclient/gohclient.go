0f8debdc62f13ade09e181a30109b33d5051bd9d
C1258882 Werberson Pereira da Silva <c1258882@bb.com.br>
2019-04-15 19:52:57 -0300

    rename Client to Default and remove unnecessary verifications on request function

--- gohclient/gohclient.go
+++ gohclient/gohclient.go
@@ -2,7 +2,6 @@ package gohclient
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/go-errors/errors"
 	"github.com/sirupsen/logrus"
 	"io"
@@ -21,19 +20,19 @@ type API interface {
 }
 
 // Default defines a struct that handles with HTTP requests for a bindman webhook client
-type Client struct {
+type Default struct {
 	// User agent used when communicating with the API
 	UserAgent string
 	// Request content type used when communicating with the API
 	ContentType string
 	Accept      string
-	baseURL     *url.URL
-	httpClient  *http.Client
+	BaseURL     *url.URL
+	HTTPClient  *http.Client
 }
 
 // New instantiates a default goh client
 // If a nil httpClient is provided, http.DefaultClient will be used.
-func NewClient(httpClient *http.Client, baseURL string) (*Client, error) {
+func New(httpClient *http.Client, baseURL string) (*Default, error) {
 	if httpClient == nil {
 		httpClient = http.DefaultClient
 	}
@@ -45,44 +44,35 @@ func NewClient(httpClient *http.Client, baseURL string) (*Client, error) {
 		return nil, err
 	}
 
-	return &Client{
-		baseURL:    parsedURL,
-		httpClient: httpClient,
+	return &Default{
+		BaseURL:    parsedURL,
+		HTTPClient: httpClient,
 	}, nil
 }
 
 // Put wraps the call to http.NewRequest apis and properly submits a new HTTP POST request
-func (c *Client) Put(url string, data []byte) (*http.Response, []byte, error) {
+func (c *Default) Put(url string, data []byte) (*http.Response, []byte, error) {
 	return c.request(url, "PUT", data)
 }
 
 // Post wraps the call to http.NewRequest apis and properly submits a new HTTP POST request
-func (c *Client) Post(url string, data []byte) (*http.Response, []byte, error) {
+func (c *Default) Post(url string, data []byte) (*http.Response, []byte, error) {
 	return c.request(url, "POST", data)
 }
 
 // Get wraps the call to http.NewRequest apis and properly submits a new HTTP GET request
-func (c *Client) Get(url string) (*http.Response, []byte, error) {
+func (c *Default) Get(url string) (*http.Response, []byte, error) {
 	return c.request(url, "GET", nil)
 }
 
 // Delete wraps the call to http.NewRequest apis and properly submits a new HTTP DELETE request
-func (c *Client) Delete(url string) (*http.Response, []byte, error) {
+func (c *Default) Delete(url string) (*http.Response, []byte, error) {
 	return c.request(url, "DELETE", nil)
 }
 
 // request defines a generic method to execute http requests
-func (c *Client) request(path, method string, body []byte) (resp *http.Response, data []byte, err error) {
-	if c.httpClient == nil {
-		err = fmt.Errorf("httpClient cannot be nil. Use gohclient.NewClient to create a correct Client instance")
-		return
-	}
-	if c.baseURL == nil {
-		err = fmt.Errorf("baseURL cannot be nil. Use gohclient.NewClient to create a correct Client instance")
-		return
-	}
-
-	u, err := c.baseURL.Parse(path)
+func (c *Default) request(path, method string, body []byte) (resp *http.Response, data []byte, err error) {
+	u, err := c.BaseURL.Parse(path)
 	if err != nil {
 		return
 	}
@@ -104,7 +94,7 @@ func (c *Client) request(path, method string, body []byte) (resp *http.Response,
 	}
 	logrus.Debugf("%v request=%v", method, req)
 
-	resp, err = c.httpClient.Do(req)
+	resp, err = c.HTTPClient.Do(req)
 	if err != nil {
 		logrus.Errorf("HTTP  %v request invocation failed. err=%v", method, err)
 		return
