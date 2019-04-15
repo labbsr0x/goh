aff1436e5f917c7ac1dcbe9fd48f721769ecb906
C1258882 Werberson Pereira da Silva <c1258882@bb.com.br>
2019-04-15 16:33:31 -0300

    add Client constructor; possibility to use a configured http.Client; Support to set UserAgent;

--- gohclient/gohclient.go
+++ gohclient/gohclient.go
@@ -2,11 +2,14 @@ package gohclient
 
 import (
 	"bytes"
+	"fmt"
+	"github.com/go-errors/errors"
+	"github.com/sirupsen/logrus"
+	"io"
 	"io/ioutil"
 	"net/http"
-	"time"
-
-	"github.com/sirupsen/logrus"
+	"net/url"
+	"strings"
 )
 
 // API defines an interface for helper methods that encapsulates http requests complexities
@@ -18,66 +21,103 @@ type API interface {
 }
 
 // Default defines a struct that handles with HTTP requests for a bindman webhook client
-type Default struct {
+type Client struct {
+	// User agent used when communicating with the API
+	UserAgent string
+	// Request content type used when communicating with the API
 	ContentType string
 	Accept      string
+	baseURL     *url.URL
+	httpClient  *http.Client
 }
 
 // New instantiates a default goh client
-func New(contentType, accept string) *Default {
-	return &Default{
-		ContentType: contentType,
-		Accept:      accept,
+// If a nil httpClient is provided, http.DefaultClient will be used.
+func NewClient(httpClient *http.Client, baseURL string) (*Client, error) {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
 	}
+	if strings.TrimSpace(baseURL) == "" {
+		return nil, errors.New("base URL cannot be an empty string")
+	}
+	parsedURL, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		baseURL:    parsedURL,
+		httpClient: httpClient,
+	}, nil
 }
 
 // Put wraps the call to http.NewRequest apis and properly submits a new HTTP POST request
-func (c *Default) Put(url string, data []byte) (*http.Response, []byte, error) {
+func (c *Client) Put(url string, data []byte) (*http.Response, []byte, error) {
 	return c.request(url, "PUT", data)
 }
 
 // Post wraps the call to http.NewRequest apis and properly submits a new HTTP POST request
-func (c *Default) Post(url string, data []byte) (*http.Response, []byte, error) {
+func (c *Client) Post(url string, data []byte) (*http.Response, []byte, error) {
 	return c.request(url, "POST", data)
 }
 
 // Get wraps the call to http.NewRequest apis and properly submits a new HTTP GET request
-func (c *Default) Get(url string) (*http.Response, []byte, error) {
+func (c *Client) Get(url string) (*http.Response, []byte, error) {
 	return c.request(url, "GET", nil)
 }
 
 // Delete wraps the call to http.NewRequest apis and properly submits a new HTTP DELETE request
-func (c *Default) Delete(url string) (*http.Response, []byte, error) {
+func (c *Client) Delete(url string) (*http.Response, []byte, error) {
 	return c.request(url, "DELETE", nil)
 }
 
-var httpDo = (&http.Client{Timeout: time.Second * 10}).Do
-
 // request defines a generic method to execute http requests
-func (c *Default) request(url, method string, payload []byte) (httpResponse *http.Response, data []byte, err error) {
-	logrus.Debugf("Request body: %v", payload)
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(payload))
-	if err == nil {
+func (c *Client) request(path, method string, body []byte) (resp *http.Response, data []byte, err error) {
+	if c.httpClient == nil {
+		err = fmt.Errorf("httpClient cannot be nil. Use gohclient.NewClient to create a correct Client instance")
+		return
+	}
+	if c.baseURL == nil {
+		err = fmt.Errorf("baseURL cannot be nil. Use gohclient.NewClient to create a correct Client instance")
+		return
+	}
+
+	u, err := c.baseURL.Parse(path)
+	if err != nil {
+		return
+	}
+
+	req, err := http.NewRequest(method, u.String(), bytes.NewBuffer(body))
+	if err != nil {
+		logrus.Errorf("HTTP request creation failed. err=%v", err)
+		return
+	}
+
+	if body != nil && strings.TrimSpace(c.ContentType) != "" {
 		req.Header.Set("Content-Type", c.ContentType)
+	}
+	if strings.TrimSpace(c.Accept) != "" {
 		req.Header.Set("Accept", c.Accept)
+	}
+	if strings.TrimSpace(c.UserAgent) != "" {
+		req.Header.Set("User-Agent", c.UserAgent)
+	}
+	logrus.Debugf("%v request=%v", method, req)
 
-		logrus.Debugf("%v request=%v", method, req)
-
-		httpResponse, err = httpDo(req)
-		if err == nil {
-			defer func() {
-				if closeError := httpResponse.Body.Close(); closeError != nil {
-					logrus.Errorf("HTTP  %v response body close invocation failed. err=%v", method, err)
-				}
-			}()
-			logrus.Debugf("Response: %v", httpResponse)
-			data, _ = ioutil.ReadAll(httpResponse.Body)
-			logrus.Debugf("Response body: %v", data)
-			return
-		}
+	resp, err = c.httpClient.Do(req)
+	if err != nil {
 		logrus.Errorf("HTTP  %v request invocation failed. err=%v", method, err)
 		return
 	}
-	logrus.Errorf("HTTP %v request creation failed. err=%v", method, err)
+	defer dClose(resp.Body)
+	logrus.Debugf("Response: %v", resp)
+	data, err = ioutil.ReadAll(resp.Body)
+	logrus.Debugf("Response body: %v", data)
 	return
+}
+
+func dClose(c io.Closer) {
+	if err := c.Close(); err != nil {
+		logrus.Errorf("HTTP response body close invocation failed. err=%v", err)
+	}
 }
