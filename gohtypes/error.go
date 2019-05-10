b8a4444f430569b0b96ff9339a8ec2385af1d8f6
Abilio Esteves <eco.abilio@gmail.com>
2019-05-10 07:52:52 -0300

    improved inner error handling

--- gohtypes/error.go
+++ gohtypes/error.go
@@ -2,8 +2,6 @@ package gohtypes
 
 import (
 	"fmt"
-	"os"
-	"strings"
 
 	"github.com/sirupsen/logrus"
 )
@@ -18,18 +16,23 @@ type Error struct {
 // Error() gives a string representing the error; also, forces the Error type to comply with the error interface
 // If the RUNTIME_MODE variable is defined and has the value DEBUG, more information is emitted to the final error message
 func (e *Error) Error() string {
-	mode := strings.Trim(os.Getenv("RUNTIME_MODE"), " ")
-	if mode == "DEBUG" {
-		return fmt.Sprintf("ERROR (%v): %s; \n Inner error: %s", e.Code, e.Message, e.Err)
+	return fmt.Sprintf("ERROR (%v): %s\n", e.Code, e.Message)
+}
+
+// InnerError gives a string with the details of an inner error, if it exists
+func (e *Error) InnerError() string {
+	iErr := ""
+	if e.Err != nil {
+		iErr = fmt.Sprintf("Inner Error: '%v'", e.Err)
 	}
-	return fmt.Sprintf("ERROR (%v): %s", e.Code, e.Message)
+	return fmt.Sprintf("ERROR (%v): %s;\n%s", e.Code, e.Message, iErr)
 }
 
 // PanicIfError is just a wrapper to a panic call that propagates a custom Error when the err property != nil
 func PanicIfError(message string, code int, err error) {
 	e := Error{Message: message, Code: code, Err: err}
 	if e.Err != nil {
-		logrus.Errorf(e.Error())
+		logrus.Errorf(e.InnerError())
 		panic(e)
 	}
 }
@@ -37,6 +40,6 @@ func PanicIfError(message string, code int, err error) {
 // Panic wraps a panic call propagating the given error parameter
 func Panic(message string, code int) {
 	e := Error{Message: message, Code: code}
-	logrus.Errorf(e.Error())
+	logrus.Errorf(e.InnerError())
 	panic(e)
 }
